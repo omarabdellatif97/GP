b727ecdecc288602bd3ad750445c75ab00d63001
Omar Hesham <omar_abdellatif97@hotmail.com>
2021-06-14 14:21:14 +0200

    installing tinymce

--- GPAngular/src/app/app.module.ts
+++ GPAngular/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EditorModule
   ],
-  providers: [],
+  providers: [{ provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
