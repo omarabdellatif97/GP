42474d20895ab75bc36ef1ef1e41843159994b09
Moamen Soroor <moamensoroor@gmail.com>
2021-06-16 23:45:10 +0200

    use async at FileController and add file delete and file exists.

--- GP_API/Controllers/FileController.cs
+++ GP_API/Controllers/FileController.cs
@@ -2,6 +2,7 @@
 using GP_API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,12 +20,14 @@ namespace GP_API.Controllers
         private readonly IFileService fileService;
         private readonly IFileEnvironment fileEnv;
         private readonly IFileRepo fileRepo;
+        private readonly ILogger<FileController> logger;
 
-        public FileController(IFileService fileService,IFileEnvironment fileEnv, IFileRepo fileRepo)
+        public FileController(IFileService fileService,IFileEnvironment fileEnv, IFileRepo fileRepo, ILogger<FileController> logger)
         {
             this.fileService = fileService;
             this.fileEnv = fileEnv;
             this.fileRepo = fileRepo;
+            this.logger = logger;
         }
 
     
@@ -35,19 +38,27 @@ namespace GP_API.Controllers
             try
             {
                 var ext = Path.GetExtension(file.FileName);
-                var url = $"{Guid.NewGuid()}.{ext}";
+                var url = $"{Guid.NewGuid()}{ext}";
                 var contentType = file.ContentType;
 
-                bool result = fileService.UploadFile(file.OpenReadStream(), url);
+                bool result = await fileService.UploadFileAsync(file.OpenReadStream(), url);
                 if (result)
                 {
-                    var created = await fileRepo.Insert(new CaseFile() {
-                        FileURL = url, 
+                    var caseFile = new CaseFile()
+                    {
+                        FileURL = url,
                         ContentType = contentType,
                         Extension = ext,
-                        FileName = file.FileName });
+                        FileName = file.FileName
+                    };
+                    var created = await fileRepo.Insert(caseFile);
                     if (created)
-                        return Ok(new { url });
+                        //return Ok(new { url });
+                        return Ok(caseFile);
+                    else
+                    {
+                        await fileService.DeleteFileAsync(url);
+                    }
                 }
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
@@ -68,7 +79,7 @@ namespace GP_API.Controllers
                 if(casefile == null)
                     return NotFound(new { message = $"File not found with ID = {id}" });
                 
-                var file = fileService.DownloadFile(Path.GetFileName(casefile.FileURL));
+                var file = await fileService.DownloadFileAsync(casefile.FileURL);
                 return Ok(File(file, $"application/{casefile.ContentType}"));
             }
             catch (Exception ex)
@@ -86,7 +97,7 @@ namespace GP_API.Controllers
                 if (casefile == null)
                     return NotFound(new { message = $"File not found with ID = {url}" });
 
-                var file = fileService.DownloadFile(Path.GetFileName(casefile.FileURL));
+                var file = await fileService.DownloadFileAsync(casefile.FileURL);
                 return Ok(File(file, $"application/{casefile.ContentType}"));
             }
             catch (Exception ex)
@@ -94,6 +105,56 @@ namespace GP_API.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
+
+
+        [HttpGet("delete")]
+        public async Task<IActionResult> deleteFileWithUrl(string url)
+        {
+            try
+            {
+                var casefile = await fileRepo.Get(url);
+                if (casefile == null)
+                    return NotFound(new { message = $"File not found with ID = {url}" });
+
+                await fileRepo.Delete(casefile.Id);
+
+
+                return Ok(casefile);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+        [HttpGet("exists")]
+        public async Task<IActionResult> IsFileExists(string url)
+        {
+            try
+            {
+                var casefile = await fileRepo.Get(url);
+                if (casefile == null)
+                    return NotFound(new { message = $"File not found with ID = {url}" });
+
+                bool result = await fileService.FileExistsAsync(casefile.FileURL);
+                
+                if(!result)
+                {
+                    
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+
+                return Ok(casefile);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+
 
     }
 }
