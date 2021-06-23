1acc0b471d597edcea2505e106e1c9daaca2f035
solaryoussef <ysobhi17@gmail.com>
2021-06-23 21:31:40 +0200

    logout addtion

--- GPAngular/src/app/app.component.ts
+++ GPAngular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { MessageService } from 'primeng/api';
-
+import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,10 @@ import { MessageService } from 'primeng/api';
 
 })
 export class AppComponent {
+
+
+constructor( public _authService:AuthService) {
+
 }
+}
+
--- GPAngular/src/app/header/header.component.ts
+++ GPAngular/src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-// import { AuthService } from '../auth/auth.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,12 @@ import { Subscription } from 'rxjs';
 })
 export class HeaderComponent implements OnInit {
   constructor(
+    public _AuthService:AuthService
   ) { }
 
   ngOnInit() {
-
   }
+
+  //IsAuth = localStorage.getItem('token');
+
 }
