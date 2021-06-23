efc710608f784c0ee52b59420e3bbb111d778295
Moamen Soroor <moamensoroor@gmail.com>
2021-06-23 17:40:40 +0200

    authentication and delete case with files

--- DAL/Repos/ICaseRepo.cs
+++ DAL/Repos/ICaseRepo.cs
@@ -11,7 +11,7 @@ namespace GP_API.Repos
         Task<Case> Get(int id);
         Task<IEnumerable<Case>> GetAll();
         Task<IEnumerable<Case>> GetAll(int page);
-        Task<IEnumerable<Case>> Search(string title, string[] tags);
+        //Task<IEnumerable<Case>> Search(string title, string[] tags);
         Task<IEnumerable<Case>> Search(SearchModel model);
         Task<bool> Delete(int id);
         Task<bool> Update(int id, Case mycase);
