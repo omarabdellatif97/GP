cc56a68791f74460e0381e823cef024d8c7baed6
Moamen Soroor <moamensoroor@gmail.com>
2021-06-23 23:27:48 +0200

    implement case delete.

--- GP_API/Controllers/AppsController.cs
+++ GP_API/Controllers/AppsController.cs
@@ -13,7 +13,7 @@ namespace GP_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class AppsController : ControllerBase
     {
         private readonly CaseContext context;
--- GP_API/Controllers/TagsController.cs
+++ GP_API/Controllers/TagsController.cs
@@ -13,7 +13,7 @@ namespace GP_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class TagsController : ControllerBase
     {
         private readonly CaseContext context;
